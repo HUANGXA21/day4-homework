82631321292a5446d63978e7270f96dee1128edf
xavier <XAVIER.HUANG@OOCL.COM>
2025-09-04 15:14:24 +0800

    refactor：rename list to frequencies

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -16,10 +16,10 @@ public class WordFrequencyGame {
             try {
                 //split the input string with 1 to n pieces of spaces
                 String[] words = inputStr.split(ANY_SPACE_SEPARATOR);
-                List<Input> list = countFrequencies(words);
-                list.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                List<Input> frequencies = countFrequencies(words);
+                frequencies.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
                 StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : list) {
+                for (Input w : frequencies) {
                     String s = w.getValue() + " " + w.getWordCount();
                     joiner.add(s);
                 }
